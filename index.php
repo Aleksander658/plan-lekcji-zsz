339c8348f728e0242a09163952fdf016c0568f32
Aleksander Obrzut <EMAIL>
2024-11-15 12:28:50 +0100

    plan lekcji

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plan lekcji ZSZ Bobowa</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Plan lekcji ZSZ Bobowa</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><h3><a href="https://zsz.bobowa.pl/">Strona Szkoły</a><a href="https://plan.zsz.bobowa.pl/">Plan lekcji</a></h3></li>
+        </ul>
+        <ul>
+            <li><p>
+            W.Szafraniec(WS)     
+            A.Iwańska(AI)    
+            G.Bogusz(GB)     
+            E.Brońska(EB)    
+            K.Flądro(KF)     
+            J.Forczek(JF)    
+            B.Forczek-Serafin(BF)    
+            J.Gagatek(GA)    
+            B.Gryzło(BG)    
+            (...)
+            </p></li>
+            <li id="klasy">
+            1SB    
+            1Ta    
+            2bSB    
+            2aSB    
+            2Ta     
+            3SB 3bSB    
+            3aB 3aSB    
+            3bT     
+            3aT     
+            4aT     
+            5aT
+            </li>
+        </ul>
+    </nav>
+    <aside>
+        <h2>Informacje</h2>
+        <p>Plan lekcji ZSZ Bobowa</p>
+    </aside>
+    <main>
+        <h2>Plan lekcji/Zastępstwa</h2>
+    </main>
+    <div>
+        <h2>Zdjęcia</h2>
+    </div>
+</body>
+</html>
