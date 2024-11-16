b0e4e64453a3ed5d51006b29ef190f9193c0cd50
Aleksander Obrzut <aleksander.obrzut@zsz.bobowa.pl>
2024-11-16 16:03:18 +0100

    poprawki małe

--- index.php
+++ index.php
@@ -14,31 +14,35 @@
         <ul>
             <li><h3><a href="https://zsz.bobowa.pl/" target="_blank">Strona Szkoły</a><a href="https://plan.zsz.bobowa.pl/" target="_blank">Plan lekcji</a></h3></li>
         </ul>
-        <ul id="nauczyciele">
-            <?php
-            $teachers1 = [
-                "W.Szafraniec(WS)", "A.Iwańska(AI)", "G.Bogusz(GB)", "E.Brońska(EB)", "K.Flądro(KF)",
-                "J.Forczek(JF)", "B.Forczek-Serafin(BF)", "J.Gagatek(GA)", "B.Gryzło(BG)", "M.Gucwa(MG)",
-                "T.Gucwa(TG)", "J.Igielski(JI)", "K.Janusz(KJ)", "B.Jasińska(BJ)", "I.Kalisz(IK)",
-                "P.Kruczek(PK)", "P.Kuk(PA)", "P.Łebski(PŁ)"
-            ];
-            foreach ($teachers1 as $teacher) {
-                echo '<li>' . htmlspecialchars($teacher, ENT_QUOTES, 'UTF-8') . '</li>';
-            }
-            ?>
-        </ul>
-        <ul id="nauczyciele2">
-            <?php
-            $teachers2 = [
-                "T.Magiera(TM)", "K.Paluch(KP)", "R.Pękala(RP)", "P.Ptaszkowski(PP)", "A.Skórska(SÓ)",
-                "T.Skórski(SK)", "J.Średniawa(JŚ)", "R.Święs(RŚ)", "S.Szafraniec(SZ)", "S.Szczepanek(SS)",
-                "P.Szura(PS)", "J.Wiejaczka(WI)", "J.Wiejaczka(JW)", "E.Wołkowicz(EW)", "J.Zborowska(JZ)"
-            ];
-            foreach ($teachers2 as $teacher) {
-                echo '<li>' . htmlspecialchars($teacher, ENT_QUOTES, 'UTF-8') . '</li>';
-            }
-            ?>
-        </ul>
+        <table id="nauczyciele">
+            <tr>
+                <?php
+                $teachers1 = [
+                    "K.Paluch(KP)", "A.Iwańska(AI)", "E.Brońska(EB)", 
+                    "J.Forczek(JF)", "B.Forczek-Serafin(BF)",  "B.Gryzło(BG)", "R.Pękala(RP)", "M.Gucwa(MG)", "A.Skórska(SÓ)",
+                     "B.Jasińska(BJ)", "I.Kalisz(IK)", "J.Wiejaczka(WI)", "J.Wiejaczka(JW)", "E.Wołkowicz(EW)", "J.Zborowska(JZ)",
+                    "P.Kruczek(PK)", "P.Kuk(PA)"
+                ];
+                foreach ($teachers1 as $teacher) {
+                    echo '<td>' . htmlspecialchars($teacher, ENT_QUOTES, 'UTF-8') . '</td>';
+                }
+                ?>
+            </tr>
+        </table>
+        <table id="nauczyciele2">
+            <tr>
+                <?php
+                $teachers2 = [
+                    "W.Szafraniec(WS)", "P.Łebski(PŁ)", "T.Magiera(TM)", "K.Flądro(KF)", "P.Ptaszkowski(PP)", "J.Gagatek(GA)",
+                    "T.Skórski(SK)", "J.Średniawa(JŚ)", "R.Święs(RŚ)", "S.Szafraniec(SZ)", "S.Szczepanek(SS)",
+                    "T.Gucwa(TG)", "J.Igielski(JI)", "K.Janusz(KJ)", "G.Bogusz(GB)", "P.Szura(PS)"
+                ];
+                foreach ($teachers2 as $teacher) {
+                    echo '<td>' . htmlspecialchars($teacher, ENT_QUOTES, 'UTF-8') . '</td>';
+                }
+                ?>
+            </tr>
+        </table>
         <ul id="klasa">
             <?php
             $classes = ["1SB", "1Ta", "2bSB", "2aSB", "2Ta", "3SB 3bSB", "3aB 3aSB", "3bT", "3aT", "4aT", "5aT"];
@@ -63,43 +67,37 @@
                 <th>Nauczyciel</th>
                 <th>Klasa</th>
             </tr>
-            <tr>
-                <td>1</td>
-                <td>8:00-8:45</td>
-                <td>W-F</td>
-                <td>W.Szafraniec</td>
-                <td>W-F</td>
-                <td>W.Szafraniec</td>
-                <td>1SB</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>8:55-9:40</td>
-                <td>W-F</td>
-                <td>W.Szafraniec</td>
-                <td>W-F</td>
-                <td>W.Szafraniec</td>
-                <td>1SB</td>
-            </tr>
-            <tr>
-                <td>3</td>
-                <td>9:50-10:35</td>
-                <td>W-F</td>
-                <td>W.Szafraniec</td>
-                <td>W-F</td>
-                <td>W.Szafraniec</td>
-                <td>1SB</td>
+            <?php
+            $lessons = [
+                ["1", "8:00-8:45", "W-F", "W.Szafraniec", "W-F", "W.Szafraniec", "1SB"],
+                ["2", "8:55-9:40", "W-F", "W.Szafraniec", "W-F", "W.Szafraniec", "1SB"],
+                ["3", "9:50-10:35", "W-F", "W.Szafraniec", "W-F", "W.Szafraniec", "1SB"],
+                ["4", "10:55-11:40", "W-F", "W.Szafraniec", "W-F", "W.Szafraniec", "1SB"],
+                ["5", "11:50-12:35", "W-F", "W.Szafraniec", "W-F", "W.Szafraniec", "1SB"],
+                ["6", "12:45-13:30", "W-F", "W.Szafraniec", "W-F", "W.Szafraniec", "1SB"],
+                ["7", "13:40-14:25", "W-F", "W.Szafraniec", "W-F", "W.Szafraniec", "1SB"],
+                ["8", "14:35-15:20", "W-F", "W.Szafraniec", "W-F", "W.Szafraniec", "1SB"]
+            ];
+            foreach ($lessons as $lesson) {
+                echo '<tr>';
+                foreach ($lesson as $item) {
+                    echo '<td>' . htmlspecialchars($item, ENT_QUOTES, 'UTF-8') . '</td>';
+                }
+                echo '</tr>';
+            }
+            ?>
         </table>
     </main>
     <section>
         <h2>Zdjęcia</h2>
         <div class="slideshow-container">
-            <img src="zdjecie1.jpg" class="active">
-            <img src="zdjecie2.jpg">
-            <img src="zdjecie3.jpg">
-            <img src="zdjecie4.jpg">
-            <img src="zdjecie5.jpg">
-            <img src="zdjecie6.jpg">
+            <?php
+            $images = ["zdjecie1.jpg", "zdjecie2.jpg", "zdjecie3.jpg", "zdjecie4.jpg", "zdjecie5.jpg", "zdjecie6.jpg"];
+            foreach ($images as $index => $image) {
+                $activeClass = $index === 0 ? 'active' : '';
+                echo '<img src="' . htmlspecialchars($image, ENT_QUOTES, 'UTF-8') . '" class="' . $activeClass . '">';
+            }
+            ?>
         </div>
     </section>
 <script src="script.js"></script>
